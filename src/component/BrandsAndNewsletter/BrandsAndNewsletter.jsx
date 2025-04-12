54c52eb116d1874ca9e65ddbf84b7ec167139483
Mahmudul Islam <mahmudul15-14270@diu.edu.bd>
2025-04-12 15:50:06 +0600

    newsletter update

--- src/component/BrandsAndNewsletter/BrandsAndNewsletter.jsx
+++ src/component/BrandsAndNewsletter/BrandsAndNewsletter.jsx
@@ -26,11 +26,11 @@ export default function BrandsAndNewsletter() {
       </div>
 
       {/* Newsletter CTA */}
-      <div className="bg-black text-white py-8 px-6 mt-12 rounded-full flex flex-col md:flex-row items-center justify-between gap-6  mx-auto">
+      <div className="md:bg-black text-white py-8 px-6 mt-12 rounded-full flex flex-col md:flex-row items-center justify-between gap-6  mx-auto">
         <h3 className="text-xl md:text-2xl font-semibold">
           Join Our Newsletter
         </h3>
-        <div className="flex items-center bg-white rounded-full px-4 py-2 w-full md:w-auto">
+        <div className="flex items-center bg-gray-400  md:bg-white rounded-full px-4 py-2 w-full md:w-auto">
           <input
             type="email"
             placeholder="Enter your email"
